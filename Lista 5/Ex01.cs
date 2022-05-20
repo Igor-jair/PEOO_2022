6e5f83cc60f06f273fdea2a88723462d9ddc00a3
Igor Jair <105497607+Igor-jair@users.noreply.github.com>
2022-05-20 18:49:33 +0000

    inseri resultados da Lista 5

---
+++ Lista 5/Ex01.cs
@@ -0,0 +1,16 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    string s = Console.ReadLine();
+    string[] v = s.Split();
+    double a = double.Parse(v[0]);
+    double b = double.Parse(v[1]);
+    double c = double.Parse(v[2]);
+    R1 = (- b + Match.Sqrt(delta))/(2 * a);
+    Console.WriteLine("formula de bhaskara");
+   
+    
+  }
+}
