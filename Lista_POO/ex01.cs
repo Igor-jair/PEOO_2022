d28f18978c7448156937946d4c15535c24bbb049
Igor Jair <105497607+Igor-jair@users.noreply.github.com>
2022-08-08 18:50:13 +0000

    inseri os códigos das questões 1 e 4

--- Lista_POO/ex01.cs
+++ Lista_POO/ex01.cs
@@ -1,13 +1,13 @@
 using System;
 
-class Circulo{
-  public double raio;
-  public double Area() {
+class Circulo {
+  public double raio;      // Atributo
+  public double Area() {   // Métodos
     return Math.PI * raio * raio;
   }
-  public double Circuferencia() {
+  public double Circunferencia() {
     return 2 * Math.PI * raio;
-  } 
+  }
 }
 
 class Program {
@@ -21,12 +21,26 @@ class Program {
     Console.WriteLine(a.raio);
     Console.WriteLine(b.raio);
     Console.WriteLine(c.raio);
-    Console.WriteLine("Digite o raio do 1ª círculo");
+    Console.WriteLine("Digite o raio do 1º círculo");
     a.raio = double.Parse(Console.ReadLine());
-    Console.WriteLine("Digite o raio do 2ª círculo");
+    Console.WriteLine("Digite o raio do 2º círculo");
     b.raio = double.Parse(Console.ReadLine());
-    Console.WriteLine("Digite o raio do 3ª círculo");
+    Console.WriteLine("Digite o raio do 3º círculo");
     c.raio = double.Parse(Console.ReadLine());
+    Console.WriteLine("Raios");
+    Console.WriteLine(a.raio);
+    Console.WriteLine(b.raio);
+    Console.WriteLine(c.raio);
+    Console.WriteLine();
+    Console.WriteLine("Áreas");
+    Console.WriteLine(a.Area());
+    Console.WriteLine(b.Area());
+    Console.WriteLine(c.Area());
+    Console.WriteLine();
+    Console.WriteLine("Circunferências");
+    Console.WriteLine(a.Circunferencia());
+    Console.WriteLine(b.Circunferencia());
+    Console.WriteLine(c.Circunferencia());
     
   }
 }
